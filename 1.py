f17a5c65456052821e22620f7f378c709f305941
lutera <39702721+lutera@users.noreply.github.com>
2018-05-29 16:23:59 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,5 +1,6 @@
 target=1000
 sum=0
+#simply add multiples of either 3 or 5 and print the sum. hard coded upper limit of 1000.
 for i in range(1,target):
     if ((i%3==0) or (i%5==0)):
         sum=sum+i
