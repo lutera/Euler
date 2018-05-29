a6ae09bf071d1a7adb86a4b4ccfea72071e626d4
lutera <39702721+lutera@users.noreply.github.com>
2018-05-29 16:25:16 +0530

    Update 2.py

--- 2.py
+++ 2.py
@@ -7,6 +7,7 @@ while True:
         break
     elif num2%2==0:
         sum=sum+num2
+    #exchanging numers using a bucket variable. could be done without a bucket. this is just for readability
     bucket=num1
     num1=num2
     num2=bucket+num2
